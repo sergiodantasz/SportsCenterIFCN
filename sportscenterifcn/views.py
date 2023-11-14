ddcec24d418bbf07df01d984fba302c671fd11b1
Sérgio Dantas <sergiodnts828@gmail.com>
2023-11-14 10:43:24 -0300

    criação dos templates e das URLs

--- sportscenterifcn/views.py
+++ sportscenterifcn/views.py
@@ -6,3 +6,38 @@ def inicio(request):
         request,
         'sportscenterifcn/pages/inicio.html'
     )
+
+
+def noticias(request):
+    return render(
+        request,
+        'sportscenterifcn/pages/noticias.html'
+    )
+
+
+def arquivos(request):
+    return render(
+        request,
+        'sportscenterifcn/pages/arquivos.html'
+    )
+
+
+def treinos(request):
+    return render(
+        request,
+        'sportscenterifcn/pages/treinos.html'
+    )
+
+
+def historia(request):
+    return render(
+        request,
+        'sportscenterifcn/pages/historia.html'
+    )
+
+
+def login(request):
+    return render(
+        request,
+        'sportscenterifcn/pages/login.html'
+    )
