603ccfa856d621e71dfa1325fbd8d72d011aecc8
Sérgio Dantas <sergiodnts828@gmail.com>
2023-11-18 19:35:01 -0300

    otimizadas algumas configurações do suap backend e alterado o nome da pasta para "suap"

--- suap/backends.py
+++ suap/backends.py
@@ -4,30 +4,30 @@ from social_core.backends.oauth import BaseOAuth2
 class SuapOAuth2(BaseOAuth2):
     name = 'suap'
     AUTHORIZATION_URL = 'https://suap.ifrn.edu.br/o/authorize/'
-    ACCESS_TOKEN_METHOD = 'POST'
     ACCESS_TOKEN_URL = 'https://suap.ifrn.edu.br/o/token/'
+    ACCESS_TOKEN_METHOD = 'POST'
     ID_KEY = 'identificacao'
     RESPONSE_TYPE = 'code'
     REDIRECT_STATE = True
     STATE_PARAMETER = True
     USER_DATA_URL = 'https://suap.ifrn.edu.br/api/eu/'
 
+    def get_user_details(self, response):
+        return {
+            'username': response.get('identificacao'),
+            'first_name': response.get('primeiro_nome'),
+            'last_name': response.get('ultimo_nome'),
+            'email': response.get('email_secundario'),
+        }
+    
     def user_data(self, access_token, *args, **kwargs):
         return self.request(
             url=self.USER_DATA_URL,
-            data={'scope': kwargs['response']['scope']},
             method='GET',
-            headers={'Authorization': 'Bearer {0}'.format(access_token)}
+            data={
+                'scope': kwargs['response']['scope'],
+            },
+            headers={
+                'Authorization': f'Bearer {access_token}',
+            },
         ).json()
-
-    def get_user_details(self, response):
-        splitted_name = response['nome'].split()
-        first_name, last_name = splitted_name[0], ''
-        if len(splitted_name) > 1:
-            last_name = splitted_name[-1]
-        return {
-            'username': response[self.ID_KEY],
-            'first_name': first_name.strip(),
-            'last_name': last_name.strip(),
-            'email': response['email'],
-        }
