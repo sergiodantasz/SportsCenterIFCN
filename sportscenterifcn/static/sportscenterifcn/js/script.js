3490fff60f35daf80d88987ebcfbacb034fee850
Sérgio Dantas <sergiodnts828@gmail.com>
2023-12-23 12:11:46 -0300

    script para corrigir a posição do rodapé e destacar a página vigente na barra de navegação

---
+++ sportscenterifcn/static/sportscenterifcn/js/script.js
@@ -0,0 +1,25 @@
+function destacarPaginaAtualBarraNavegacao() {
+  let url = window.location.pathname;
+  let nomePaginaAtual;
+  if (url == '/') {
+    nomePaginaAtual = 'inicio';
+  } else {
+    nomePaginaAtual = url.split('/')[1];
+  }
+  let opcaoBarraNavegacao = document.getElementById(nomePaginaAtual);
+  if (opcaoBarraNavegacao) {
+    opcaoBarraNavegacao.classList.add('pagina-atual');
+  }
+}
+
+function atualizarAlturaRodape() {
+  let rodape = document.getElementById('rodape');
+  let altura = rodape.getBoundingClientRect().height;
+  console.log(altura);
+  rodape.style.bottom = -altura + 'px';
+}
+
+window.addEventListener('resize', atualizarAlturaRodape);
+
+destacarPaginaAtualBarraNavegacao();
+atualizarAlturaRodape();
