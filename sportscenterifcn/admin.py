45ca9f346ffd143742fc1a18bc58117d0551efff
Sérgio Dantas <sergiodnts828@gmail.com>
2023-12-25 13:35:18 -0300

    adicionados os models ao admin.py

--- sportscenterifcn/admin.py
+++ sportscenterifcn/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from sportscenterifcn import models
+
+
+@admin.register(models.Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Treino)
+class TreinoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Noticia)
+class NoticiaAdmin(admin.ModelAdmin):
+    pass
