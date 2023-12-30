c59e2e22a8d621b8fbec00092b370b7d54e247ec
Sérgio Dantas <sergiodnts828@gmail.com>
2023-12-30 01:38:00 -0300

    removidos arquivos desnecessários

--- utils/noticia.py
+++
@@ -1,7 +0,0 @@
-from os.path import splitext
-
-
-def caminho_capa_noticia(instance, filename):
-    arquivo, extensao = splitext(filename)
-    arquivo_renomeado = instance.slug + extensao
-    return f'noticias/capas/{arquivo_renomeado}'
