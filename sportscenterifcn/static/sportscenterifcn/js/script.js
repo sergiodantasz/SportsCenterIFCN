4d7a546555965704443b748367f5a50e6d76fc25
Sérgio Dantas <sergiodnts828@gmail.com>
2023-12-30 01:46:50 -0300

    criado menu mobile

--- sportscenterifcn/static/sportscenterifcn/js/script.js
+++ sportscenterifcn/static/sportscenterifcn/js/script.js
@@ -1,3 +1,11 @@
+let menuMobile = document.querySelector('.barra-navegacao .menu-mobile');
+let menu = document.querySelector('.barra-navegacao .menu');
+
+menuMobile.addEventListener('click', () => {
+  menuMobile.classList.toggle('active');
+  menu.classList.toggle('active');
+})
+
 function destacarPaginaAtualBarraNavegacao() {
   let url = window.location.pathname;
   let nomePaginaAtual;
@@ -12,13 +20,15 @@ function destacarPaginaAtualBarraNavegacao() {
   }
 }
 
-function atualizarAlturaRodape() {
+function atualizarDimensoesRodape() {
   let rodape = document.getElementById('rodape');
   let altura = rodape.getBoundingClientRect().height;
   rodape.style.bottom = -altura + 'px';
+  let larguraPagina = document.documentElement.clientWidth;
+  rodape.style.width = larguraPagina + 'px';
 }
 
-window.addEventListener('resize', atualizarAlturaRodape);
+window.addEventListener('resize', atualizarDimensoesRodape);
 
 destacarPaginaAtualBarraNavegacao();
-atualizarAlturaRodape();
+atualizarDimensoesRodape();
